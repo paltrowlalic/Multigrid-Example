096d077a6497986b094a5dea03e49aa473a9bc8e
Jiangnan Zhang <jiangnan.zhang@outlook.com>
2025-06-14 00:42:55 +0100

    Initial Commit

---
+++ Multigrid Example/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Multigrid Example
+//
+//  Created by Jiangnan Zhang on 14/06/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
